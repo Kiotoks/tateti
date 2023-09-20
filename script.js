70ef68deb34895e8729fb0106439f040e958c043
kiotoks <nikogr2005@gmail.com>
2023-09-20 13:40:37 +0200

    Terminado

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
-var estado = [
-[0,0,0],
-[0,0,0],
-[0,0,0]
-]
+let jugadorUno = true;
+let celdas = document.getElementsByClassName("cell");
+const botonReiniciar = document.querySelector('#reiniciar');
+botonReiniciar.addEventListener('click', reiniciar);
 
-function sex(){
-    document.getElementById("mensaje").innerHTML = "odioa los gays";
+for (let index = 0; index < celdas.length; index++) {
+   celdas[index].addEventListener("click", adentroCelda);
 }
+
+function reiniciar(){
+   for (let i = 0; i < celdas.length; i++) {
+      celdas[i].innerHTML = "";
+      celdas[index].addEventListener("click", adentroCelda);
+   }
+   showWinner("")
+   jugadorUno = True;
+}
+
+function adentroCelda(e) {
+   let cellValue = e.target.innerHTML;/**se fija que hay en la celdad onde se toco si hay algo no hace nada y si no hay pone x / o */
+   if (!cellValue.length) {
+      e.target.innerHTML = jugadorUno ? "X" : "O";
+      jugadorUno = !jugadorUno;
+
+      ganar(0, 1, 2);
+      ganar(3, 4, 5);
+      ganar(6, 7, 8);
+      ganar(0, 3, 6);
+      ganar(1, 4, 7);
+      ganar(2, 5, 8);
+      ganar(0, 4, 8);
+      ganar(6, 4, 2);
+      
+   }
+}
+
+function ganar(c1, c2, c3) {
+   if (
+      celdas[c1].innerHTML.length &&
+      celdas[c1].innerHTML == celdas[c2].innerHTML &&
+      celdas[c2].innerHTML == celdas[c3].innerHTML
+   ) {
+      showWinner("Gano: " + celdas[c1].innerHTML);
+   }
+}
+
+function showWinner(player) {
+   document.querySelector("#resultado").innerHTML = player;
+   for (let index = 0; index < celdas.length; index++) {
+      celdas[index].removeEventListener("click", adentroCelda);
+   }
+   
+}
+
+/** restar boton falta agregar */
+
+/** el inner html es para obbtener cosas del html en esta caso la celda// el query selector es para seleccionar y devuelvo la primera cosa de un domento y mas el all devuelve todo */
+/**addeventlisteners le dice al js que vas a clakear en este caso getelementsbyclassname guarda en la variable cell todas las celdas del html*/
